a12f0287f146aff8a2697ee7a8a510c69afa2345
vishal-936 <76116570+vishal-936@users.noreply.github.com>
2020-12-25 19:05:00 +0530

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+import random
+
+spam_text=["You are getting Spam","Nice work","You are web devoloper Now","this is whatsapp spam"]
+NumberOfTimes=100
+
+for _ in range(NumberOfTimes):
+    pyautogui.typewrite(random.choice(spam_text))
+    pyautogui.press('left')
+    # pyautogui.press('enter')
+    time.sleep(0.1)
+    
+    
+
+
+    
